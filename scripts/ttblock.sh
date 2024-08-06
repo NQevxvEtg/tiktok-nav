a410bc894a0700d6dc76938400902fb48894ad9e
NQevxvEtg <87945683+NQevxvEtg@users.noreply.github.com>
2024-08-06 16:57:03 +0000

    New commit

--- scripts/ttblock.sh
+++ scripts/ttblock.sh
@@ -4,15 +4,15 @@ path=/path
 
 $path/platform-tools/adb shell input tap 985 1156
 # $path/platform-tools/adb shell input swipe 925 2165 303 2165 250
-sleep 0.5
+sleep 0.6
 $path/platform-tools/adb shell input tap 1000 192
-sleep 0.5
+sleep 0.6
 $path/platform-tools/adb shell input swipe 925 2165 303 2165 250
-sleep 0.5
+sleep 0.6
 $path/platform-tools/adb shell input tap 568 2165
-sleep 0.5
+sleep 0.6
 $path/platform-tools/adb shell input tap 747 1670
-sleep 0.5
+sleep 0.6
 $path/platform-tools/adb shell input tap 94 202
-sleep 0.5
+sleep 0.6
 $path/platform-tools/adb shell input swipe 555 1850 555 1000 250
